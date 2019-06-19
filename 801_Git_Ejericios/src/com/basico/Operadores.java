b252194cf027c3ae12c836ad2cb0187daa561165
Daniel <daniel.seguridadjava@gmail.com>
2019-06-19 09:47:36 +0200

    primer commit de Ejericios de Java

---
+++ 801_Git_Ejericios/src/com/basico/Operadores.java
@@ -0,0 +1,16 @@
+package com.basico;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		int n1 = 15;
+		int n2 = 16;
+		System.out.println("la suma es " +(n1+n2));
+		System.out.println("la resta es "+(n1-n2));
+		System.out.println("la * es "+(n1*n2));
+		System.out.println("la / es "+(n1/n2));
+		System.out.println("el % es "+(n1%n2));
+	}
+
+}

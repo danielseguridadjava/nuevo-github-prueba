6b986b6271112cb5ffa01bf36114dc81900422e2
Daniel <daniel.seguridadjava@gmail.com>
2019-06-19 09:50:35 +0200

    cambios de numeros

--- 801_Git_Ejericios/src/com/basico/Operadores.java
+++ 801_Git_Ejericios/src/com/basico/Operadores.java
@@ -4,8 +4,8 @@ public class Operadores {
 
 	public static void main(String[] args) {
 		
-		int n1 = 15;
-		int n2 = 16;
+		int n1 = 100;
+		int n2 = 50;
 		System.out.println("la suma es " +(n1+n2));
 		System.out.println("la resta es "+(n1-n2));
 		System.out.println("la * es "+(n1*n2));
